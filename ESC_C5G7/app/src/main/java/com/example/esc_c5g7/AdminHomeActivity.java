bbb82e10a55ed9f123208b0a7398b875210bfd7f
rmurarishetti <64725000+rmurarishetti@users.noreply.github.com>
2022-07-22 10:20:38 +0800

    Admin Home Activity Integrated

---
+++ ESC_C5G7/app/src/main/java/com/example/esc_c5g7/AdminHomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.esc_c5g7;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminHomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adminhome);
+    }
+}
